18b41aaab34bf0ac279c0c4bd6f12bd6797838da
AnasAhmad12 <anusahmad2014@gmail.com>
2023-07-08 00:40:45 +0500

    myform changes

--- modules/eraxon_myform/controllers/eraxon_myform.php
+++ modules/eraxon_myform/controllers/eraxon_myform.php
@@ -5,7 +5,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 /**
  * 
  */
-class Eraxon_myform extends AdminController
+class eraxon_myform extends AdminController
 {
 	
 	function __construct()
